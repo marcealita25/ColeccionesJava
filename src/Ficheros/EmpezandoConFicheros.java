63974adcdcdb5514ccf4d85c8667421e8479e9e5
marcealita25 <conialto@gmail.com>
2025-03-06 16:31:31 +0100

    cambios ficheros

---
+++ src/Ficheros/EmpezandoConFicheros.java
@@ -0,0 +1,66 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class EmpezandoConFicheros {
+    public static void main(String[] args) {
+       
+        
+        
+        File directorio1  = new File("C:\\Users\\Tardes\\Ejercicioficheros\\imagenes\\");
+        directorio1.mkdir();
+         
+        String [] directorios = {}
+        File directorioImagenes = new File ("imagenes");
+
+        File directorioDocumentos  = new File ("documentos");
+        File directorioOtros      = new File ("otros");
+
+        directorioImagenes.mkdir();
+        directorioDocumentos.mkdir(); 
+        directorioOtros.mkdir();
+
+            Scanner scanner = new Scanner(System.in);
+            String [] Filanames = new String[5];
+
+        System.out.println("Introduce 5 nombres de archivo con su extension");
+
+         for (int i = 0; i < 10; i++) {
+            String nombreArchivo = scanner.nextLine();
+            
+            File archivo = new File(nombreArchivo);
+
+            if (! archivo.exists()){
+                try {
+                    archivo.createNewFile();
+                } catch (IOException e) {
+                    System.out.println("Error al ");
+                }
+
+
+
+            }
+         }
+        
+
+
+
+        
+        
+
+    } 
+
+    
+    
+
+    
+}
+
+/*PRIMERO CREAR 3 DIRECTORIOS,
+imagenes,documentos,otros.
+PREGUNTAR POR 5 NOMBRES DE Ficheros,si el fichero tienes una excepcion de una imagen JPG,GIF, guarda en la carpeta de imagenes.
+si el fichero es un txt en documentos y sino es una imagen ni un documento lo ponemos en otros.
+coger todos los ficheros que hay en otros y reenombrarlos con vuestro nombre y un contador manteniendo la extension que tiene el fichero.*/
